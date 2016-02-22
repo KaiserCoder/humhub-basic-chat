4d9f57a93df948b1c84c359efe94b2926abe2510
root <root@localhost.localdomain>
2016-02-22 13:40:44 +0100

    Add video, images and color support

--- lib/BBCodes.php
+++ lib/BBCodes.php
@@ -16,129 +16,50 @@ class BBCodes
     const PATTERN_ITEM = '@\s*\[ITEM\](.*)\[/ITEM\]\s*@i';
     const PATTERN_LIST = '@\s*\[LIST\](.*)\[/LIST\]\s*@is';
     const PATTERN_LINK = '#((http|ftp)s?://)?(([a-z][a-z0-9-]*\.)+)?[a-z][a-z0-9-]*\.([a-z]{2,6})(/[^\s]*)?#is';
-    const OPT_URLS = 1;
-    const OPT_BASE = 2;
-    const OPT_CODE = 4;
+    const PATTERN_COLOR = '@\[COLOR=([\w#]+)\](.*)\[/COLOR\]@is';
+    const PATTERN_VIDEO = '@\[VIDEO\](https\:\/\/www\.youtube\.com\/watch\?v=([\w]+))\[/VIDEO\]@is';
 
-    // Clean Options
-    const OPT_LIST = 8;
-    const OPT_QUOTE = 16;
-    const OPT_BASIC = 31;
-    const OPT_TITLE = 32;
-    const OPT_IMG = 64;
-    const OPT_NOPARSE = 128; // 31 = 1|2|4|8|16
-    const OPT_FULL = 255;
-    private $CodeReplace = [];
-    private $NoParseReplace = [];
-    private $str; // 255 = 1|2|4|8|16|32|64|128
+    private $str;
 
-    /**
-     * Same as calling clean($input, BBCodes::OPT_FULL)
-     *
-     * @param string $input
-     *
-     * @return string
-     */
-    public function fullClean($input)
-    {
-        return $this->clean($input, self::OPT_FULL);
-    }
-
-    /**
-     * @param     $input
-     * @param int $opt
-     *
-     * @return string
-     */
-    public function clean($input, $opt = self::OPT_BASIC)
+    public function clean($input)
     {
         $this->str = $input;
 
-        if (self::OPT_NOPARSE & $opt) {
-            $this->findNoParse();
-        }
-        if (self::OPT_CODE & $opt) {
-            $this->findCode();
-        }
-        if (self::OPT_LIST & $opt) {
-            $this->parseList();
-        }
-        if (self::OPT_BASE & $opt) {
-            $this->parseBase();
-        }
-        if (self::OPT_TITLE & $opt) {
-            $this->parseTitle();
-        }
-        if (self::OPT_URLS & $opt) {
-            $this->parseURL();
-        }
-        if (self::OPT_IMG & $opt) {
-            $this->parseImage();
-        }
-        if (self::OPT_QUOTE & $opt) {
-            $this->parseQuote();
-        }
-        if (self::OPT_URLS & $opt) {
-            $this->parseURLBlock();
-        }
+        $this->parseBase();
+        $this->parseURL();
+        $this->parseQuote();
+        $this->parseImage();
+        $this->parseColor();
+        $this->parseVideo();
 
         $this->str = nl2br($this->str);
         $this->str = preg_replace('#<br( /)?>\s*<br( /)?>#', '</p><p>', $this->str);
 
-        if (self::OPT_CODE & $opt) {
-            $this->parseCode();
-        }
-        if (self::OPT_NOPARSE & $opt) {
-            $this->parseNoParse();
-        }
-
         return preg_replace('#javascript([\s]*):#', 'ponyscript:', $this->str);
     }
 
-    private function findNoParse()
+    private function parseVideo()
     {
-        //*/ Set up replace array for NOPARSE tags
-        if (preg_match_all(self::PATTERN_NO_PARSE, $this->str, $matches)) {
-            foreach ($matches[1] as $k => $v) {
-                $this->NoParseReplace[$k] = $v;
-            }
+        while (preg_match(self::PATTERN_VIDEO, $this->str)) {
+            $this->str = preg_replace_callback(self::PATTERN_VIDEO, [static::class, 'videoToHTML'], $this->str);
         }
     }
 
-    private function findCode()
+    private static function videoToHTML($v)
     {
-        //*/ Set up replace array for code tags
-        if (preg_match_all(self::PATTERN_CODE, $this->str, $matches)) {
-            foreach ($matches[3] as $k => $v) {
-                if (!empty($matches[2][$k]) && class_exists(GenSynth::class)) {
-                    $v = GenSynth::highlight_string($v, strtolower($matches[2][$k]), true, GenSynth::OPT_HEADER_DIV & GenSynth::OPT_LINE_NUMBERS_FANCY & GenSynth::OPT_CAPS_NO_CHANGE);
-                    $this->CodeReplace[$k] = '<div class="bbcodes_code_wrapper"><span class="bbcodes_code_language">' . $matches[2][$k] . ' Code:</span>' . "\n"
-                                             . '<div class="bbcodes_code">' . $v . '</div></div>';
-                }
-                else {
-                    $v = "<pre>$v</pre>";
-                    $this->CodeReplace[$k] = '<div class="bbcodes_code_wrapper"><span class="bbcodes_code_language">Code:</span>' . "\n"
-                                             . '<div class="bbcodes_code">' . $v . '</div></div>';
-                }
-            }
+        return '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $v[2] . '" frameborder="0" allowfullscreen></iframe>';
+    }
+
+    private function parseColor()
+    {
+        while (preg_match(self::PATTERN_COLOR, $this->str)) {
+            $this->str = preg_replace_callback(self::PATTERN_COLOR, [static::class, 'colorToHTML'], $this->str);
         }
     }
 
-    private static function listToHTML($v)
+    private static function colorToHTML($v)
     {
-        if (preg_match(BBCodes::PATTERN_ITEM, $v[1])) {
-            return '<ul>' . preg_replace(BBCodes::PATTERN_ITEM, '<li>\\1</li>', $v[1]) . '</ul>';
-        }
-        else {
-            return $v[0];
-        }
-    }
-
-    private function parseList()
-    {
-        while (preg_match(self::PATTERN_LIST, $this->str)) {
-            $this->str = preg_replace_callback(self::PATTERN_LIST, [static::class, 'listToHTML'], $this->str);
-        }
+        return '<p style="color:' . $v[1] . '">' . $v[2] . '</p>';
     }
 
     private static function baseToHTML($v)
@@ -156,26 +77,6 @@ class BBCodes
     {
         while (preg_match(self::PATTERN_BASE, $this->str)) {
             $this->str = preg_replace_callback(self::PATTERN_BASE, [static::class, 'baseToHTML'], $this->str);
-        }
-    }
-
-    private static function titleToHTML($v)
-    {
-        $t = strtolower($v[1]);
-        switch ($t) {
-            case 'title':
-                return "<h2>$v[3]</h2>";
-            case 'subtitle':
-                return "<h3>$v[3]</h3>";
-            default:
-                return $v[3];
-        }
-    }
-
-    private function parseTitle()
-    {
-        while (preg_match(self::PATTERN_TITLE, $this->str)) {
-            $this->str = preg_replace_callback(self::PATTERN_TITLE, [static::class, 'titleToHTML'], $this->str);
         }
     }
 
@@ -246,54 +147,4 @@ class BBCodes
         }
     }
 
-    private function parseURLBlock()
-    {
-        if (preg_match_all(self::PATTERN_URL_BLOCK, $this->str, $matches)) {
-            $replace = [];
-            foreach ($matches[1] as $k => $v) {
-                $replace[$matches[0][$k]] = $this->findURLs($v);
-            }
-            $this->str = strtr($this->str, $replace);
-        }
-    }
-
-    private function urlToLink($v)
-    {
-        return '<a href="' . $v[0] . '">' . BBCodes::shortenURL($v[0]) . '</a>';
-    }
-
-    private function findURLs($str)
-    {
-        return preg_replace_callback(self::PATTERN_LINK, [static::class, 'urlToLink'], $str);
-    }
-
-    private function parseCode()
-    {
-        //*/ Set up replace array for code tags (post BB filtering, so the BB tags don't get changed when IN code)
-        if (preg_match_all(self::PATTERN_CODE, $this->str, $matches)) {
-            $CodeFind = [];
-            foreach ($matches[0] as $k => $v) {
-                $CodeFind[$k] = $v;
-            }
-            $this->str = str_replace($CodeFind, $this->CodeReplace, $this->str);
-            foreach ($this->CodeReplace as $k => $v) {
-                unset($this->CodeReplace[$k]);
-            }
-        }
-    }
-
-    private function parseNoParse()
-    {
-        //*/ Set up replace array for NOPARSE tags (post BB filtering)
-        if (preg_match_all(self::PATTERN_NO_PARSE, $this->str, $matches)) {
-            $NOPARSEfind = [];
-            foreach ($matches[0] as $k => $v) {
-                $NOPARSEfind[$k] = $v;
-            }
-            $this->str = str_replace($NOPARSEfind, $this->NoParseReplace, $this->str);
-            foreach ($this->NoParseReplace as $k => $v) {
-                unset($this->NoParseReplace[$k]);
-            }
-        }
-    }
 }
